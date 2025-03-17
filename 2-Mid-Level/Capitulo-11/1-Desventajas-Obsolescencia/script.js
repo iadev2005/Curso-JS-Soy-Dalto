47d87f533db64c58adc4bd32c4f05cbe916a8dc1
iadev2005 <idaliendres.23@est.ucab.edu.ve>
2025-03-17 15:21:19 -0400

    Terminado el capitulo 11

---
+++ 2-Mid-Level/Capitulo-11/1-Desventajas-Obsolescencia/script.js
@@ -0,0 +1,62 @@
+/**
+ * Desventajas de trabajar de manera obsoleta
+ * - ¿Cuándo se vuelve algo obsoleto?
+ *      - Deprecated / Obsoleto, aparece en la documentación oficial
+ *      - Inútil
+ *      - No recomendado
+ *      - Con Bugs o Fallos
+ *      - Está por ser reemplazado
+ *      - Hay mejores formas de hacerlo
+ * - Los efectos negativos (aplicado a métodos, clases y propiedades)
+ *      - Uso excesivo de recursos
+ *      - Código con Bugs o Fallos
+ *      - Código innecesariamente largo
+ *      - SEO
+ * - ¿Cómo verificar si está obsoleto?
+ *      - 1 de cada 2 WEBS utiliza librerías de javascript obsoletas
+ *      - Verificar si tienen o usan funciones, métodos, objetos o metodologías obsoletas
+ *      - Verificar en los sitios basados en estándares oficiales
+ */
+
+/**
+ * Lista de métodos transformadores de cadenas obsoletos
+ * - big() - Crea un <big>
+ * - blink() - Crea un <blink>
+ * - bold() - Crea un <b>
+ * - fixed() - Crea un <tt>
+ * - fontcolor() - Crea un <fontcolor>
+ * - fontsize() - Crea un <font>
+ * - italics() - Crea un <i>
+ * - anchor() - Crea un <a> (sin href)
+ * - link() - Crea un <a> (con href)
+ * - small() - Crea un <small>
+ * - strike() - Crea un <strike>
+ * - sub() - Crea un <sub>
+ * - sup() - Crea un <sup>
+ */
+
+/**
+ * Métodos específicos obsoletos
+ * - observe()
+ * - unobserve()
+ * - getNotifier()
+ * - watch()
+ * - unwatch()
+ * - toSource()
+ * - escape() (la función)
+ * - unescape() (la función)
+ * - uneval como método
+ * - eval como método
+ */
+
+/**
+ * Detectar navegadores obsoletos
+ * - Detección del navegador y cobertura a múltiples navegadores
+ */
+
+/**
+ * Recomendaciones
+ * - Ver como buenos programadores hacen nuestro código
+ * - Revisar la documentación
+ * - Ver si el rendimiento es óptimo
+ */
